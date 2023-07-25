c45f61b11ad19b2d5be2aacb6e1275bf2b0a3c82
amol <amlo1232001@gmail.com>
2023-07-26 00:37:31 +0530

    feat: added header

--- app/page.js
+++ app/page.js
@@ -8,12 +8,21 @@ function getBlog() {
 }
 
 export default function Home() {
-  const files = getBlog();
+  // const files = getBlog();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Amol
-      { files.map((file, idx) => <div key={idx}>{ file }</div>) }
-    </main>
+    <section className="text-gray-600 body-font dark:bg-gray-900">
+      <div className="container mx-auto flex px-5 pb-24 pt-20 items-center justify-center flex-col">
+        <img className="lg:w-1/6 md:w-2/6 sm:w-2/6 w-3/6 mb-10 object-cover object-center rounded" alt="hero" src="/favicon.ico" />
+          <div className="text-center lg:w-2/3 w-full">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-white">Microdosing synth tattooed vexillologist</h1>
+            <p className="mb-8 leading-relaxed dark:text-gray-300">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-blue-700 border-0 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded sm:text-lg text-base">Lets Started</button>
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded sm:text-lg text-base">Portfolio</button>
+            </div>
+          </div>
+      </div>
+    </section>
   )
 }
