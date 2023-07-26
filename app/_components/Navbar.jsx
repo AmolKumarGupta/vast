26e944b3b7a374ecf377642cf8f47136be5b4182
amol <amlo1232001@gmail.com>
2023-07-27 01:33:27 +0530

    feat: blog name Vast

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -17,7 +17,7 @@ export default function NavBar() {
                     unoptimized={true}
                     className="h-8 mr-3" 
                 />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Blog</span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Vast</span>
             </a>
             <button onClick={() => setOpenNavBar((prev) => !prev)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
--- app/page.js
+++ app/page.js
@@ -13,8 +13,10 @@ export default function Home() {
 					className="lg:w-1/6 md:w-2/6 sm:w-2/6 w-3/6 mb-10 object-cover object-center rounded"
 				/>
 				<div className="text-center lg:w-2/3 w-full">
-					<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-white">Microdosing synth tattooed vexillologist</h1>
-					<p className="mb-8 leading-relaxed dark:text-gray-300">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+					<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-white">Simple And Programatic Blog</h1>
+					<p className="mb-8 leading-relaxed dark:text-gray-300">
+						A way to learn and keep things in one place in this vast world of code
+					</p>
 					<div className="flex justify-center">
 						<button className="inline-flex text-white bg-blue-700 border-0 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded sm:text-lg text-base">Lets Started</button>
 						<button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded sm:text-lg text-base">Portfolio</button>
