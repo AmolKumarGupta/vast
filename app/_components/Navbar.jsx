353c99ae0b5491086a84495dcc99fb17794bb429
amol <amlo1232001@gmail.com>
2023-07-29 01:19:18 +0530

    feat: add nav links

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -15,6 +15,7 @@ export default function NavBar() {
                     width={30}
                     height={30}
                     unoptimized={true}
+                    priority={false}
                     className="h-8 mr-3" 
                 />
                 {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Vast</span> */}
@@ -28,10 +29,10 @@ export default function NavBar() {
             <div className={`${isOpenNavBar ? '' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
                 <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                        <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                        <a href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
                     </li>
                     <li>
-                        <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
+                        <a href="/blogs" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
                     </li>
                     <li>
                         <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
---
+++ app/blogs/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Blogs() {
+    return (
+        <main className='container mx-auto px-3 max-w-4xl'>
+            Blogs
+        </main>
+    )
+}
