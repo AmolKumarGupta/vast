96e1fe328b914c858244d948727fe1a78ef0de39
amol <amlo1232001@gmail.com>
2023-08-01 02:43:40 +0530

    feat: docs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    trailingSlash: true
+    trailingSlash: true,
+    distDir: 'docs',
 }
 
 module.exports = nextConfig
