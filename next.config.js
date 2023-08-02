ba7fb5791140ffbc22130ab3738c243815276021
amol <amlo1232001@gmail.com>
2023-08-03 00:03:52 +0530

    fix: site links

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
     distDir: 'docs',
+    assetPrefix: process.env.NODE_ENV === "production" ? '/blog' : '',
+    env: {
+        ASSET_PREFIX: process.env.NODE_ENV === "production" ? '/blog' : '',
+    },
 }
 
 module.exports = nextConfig
